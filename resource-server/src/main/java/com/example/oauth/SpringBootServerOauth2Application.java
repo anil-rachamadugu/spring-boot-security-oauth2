9ca9e98c683aaa4e37d016ea279052137b572018
Anil kumar Rachamadugu <anilkumar.r@techmojo.in>
2020-02-11 17:40:18 +0530

    Initial changes into master

---
+++ resource-server/src/main/java/com/example/oauth/SpringBootServerOauth2Application.java
@@ -0,0 +1,13 @@
+package com.example.oauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServerOauth2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServerOauth2Application.class, args);
+	}
+
+}
---
+++ resource-server/src/main/java/com/example/oauth/config/AuthorizationServerConfig.java
@@ -0,0 +1,17 @@
+package com.example.oauth.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+
+@Configuration
+@EnableAuthorizationServer
+public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
+    @Override
+    public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
+        clients.inMemory().withClient("demo_client").secret("{noop}clientsecret").authorizedGrantTypes("authorization_code")
+            .scopes("read").authorities("CLIENT");
+    }
+}
